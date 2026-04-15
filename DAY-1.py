1079f608ac617fc1ba527dcf58ee81fc7606867b
mangala-sai-pavani <saipavani175@gmail.com>
2026-04-15 15:49:49 +0530

    Add files via upload

---
+++ DAY-1.py
@@ -0,0 +1,12 @@
+"""
+->What is python? : High Level Language(No need to worry about low level management like memeory allocation)
+-> It never saves permanently ,but only save at run time.
+-> Object Oriented Language(OOPs)
+-> Dynamically-typed language(No need to mention data type as C)
+
+"""
+#Adding 2 Numbers
+a= int(input("Enter a number1: "))
+b= int(input("Enter a number2: "))
+print(f"a={a}")
+print(f"b={b}")
